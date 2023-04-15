3fde2cfb81b55ae39cb8e0ed1dbcda0c8cd688e1
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-04-15 16:35:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+    float a=0, b=0, x=0, y=0;
+    cin>>a;
+    cin>> b;
+    cin>> x;
+    cin>> y;
+    float z1= a*x-b*y, z2=a*x*x- b*y, z3=a*x + b*y*y;
+    if ((x<0) && (y>0)){
+        cout<< z1;
+    } else if ((x>=0)&&(y<=0)){
+        cout<< z2;
+    } else {
+        cout<<z3;
+    }
    return 0;
 }
